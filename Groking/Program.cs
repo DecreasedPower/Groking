0458820b32d135bd633b4476b63fc77d0af5d380
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2024-08-02 14:05:23 +0300

    add binary search

--- Groking/Program.cs
+++ Groking/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = [1, 3, 5, 7, 9];
+        Console.WriteLine(BinarySearch(array, 1));
+        Console.WriteLine(BinarySearch(array, 3));
+        Console.WriteLine(BinarySearch(array, 5));
+        Console.WriteLine(BinarySearch(array, 7));
+        Console.WriteLine(BinarySearch(array, 9));
+    }
+
+    static int BinarySearch(int[] array, int item)
+    {
+        int low = 0;
+        int high = array.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int guess = array[mid];
+
+            if (guess < item)
+            {
+                low = mid + 1;
+            }
+            else if (guess > item)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return -1;
     }
 }
